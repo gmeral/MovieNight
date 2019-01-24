64db6934aa5ac9f448602c49ff0c5eb660bc922f
guillaume <guillaume@2denker.de>
2019-01-24 12:26:47 +0100

    clear index page layout, add empty list message and link to search

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-layout column>
-    <v-flex shrink>
-      <v-text-field label="Enter the movie IMDB id" v-model="userMovieInput"/>
-      <v-btn @click="fetchMovie(userMovieInput)">Ok</v-btn>
-    </v-flex>
+  <div>
     <v-layout v-for="(movie, index) in movies" :key="index" shrink class="mt-3">
-      <img :src="movie.posterPath()"/>
+      <img :src="movie.posterPath()" />
       <v-layout column class="ml-3">
         <h1 class="mt-3">{{movie.title()}}</h1>
         <div class="mt-3">{{movie.description()}}</div>
       </v-layout>
     </v-layout>
-  </v-layout>
+    <template v-if="movies.length == 0">
+      <div>No movie has been selected</div>
+      <nuxt-link to="/search">Go to search</nuxt-link>
+    </template>
+  </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
